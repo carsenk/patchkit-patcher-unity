727deb846cbcb4bf895aa5f413ccc1a32588b329
Tomasz Jaworski <tomasz.jaworski98@gmail.com>
2017-06-26 23:47:32 +0200

    Add UnityDebugLogWriter class

---
+++ Assets/PatchKit Patcher/Scripts/Unity/UnityDebugLogWriter.cs
@@ -0,0 +1,22 @@
+﻿using PatchKit.Patcher.Debug;
+
+namespace PatchKit.Patcher.Unity
+{
+    public class UnityDebugLogWriter : IDebugLogWriter
+    {
+        public void Log(string message)
+        {
+            UnityEngine.Debug.Log(message);
+        }
+
+        public void LogWarning(string message)
+        {
+            UnityEngine.Debug.LogWarning(message);
+        }
+
+        public void LogError(string message)
+        {
+            UnityEngine.Debug.LogError(message);
+        }
+    }
+}
