b38898e2f33a7026ec346517faca7e5f38dc437f
Tomasz Jaworski <tomasz.jaworski98@gmail.com>
2017-06-26 23:47:21 +0200

    Add IDebugLogWriter interface

---
+++ Assets/PatchKit Patcher/Scripts/Debug/IDebugLogWriter.cs
@@ -0,0 +1,11 @@
+﻿namespace PatchKit.Patcher.Debug
+{
+    public interface IDebugLogWriter
+    {
+        void Log(string message);
+
+        void LogWarning(string message);
+
+        void LogError(string message);
+    }
+}
