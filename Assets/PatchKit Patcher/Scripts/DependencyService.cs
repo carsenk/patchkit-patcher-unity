ee664a9d1c6ef1072b5eb1118c050a41ab6d5ae3
Tomasz Jaworski <tomasz.jaworski98@gmail.com>
2017-06-26 23:39:47 +0200

    Add DependencyService

---
+++ Assets/PatchKit Patcher/Scripts/DependencyService.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.Practices.Unity;
+using PatchKit.Patcher.AppData.Local;
+using PatchKit.Patcher.AppData.Remote.Downloaders;
+using PatchKit.Patcher.Unity;
+
+namespace PatchKit.Patcher
+{
+    public static class DependencyService
+    {
+        public static readonly IUnityContainer Container;
+
+        static DependencyService()
+        {
+            Container = new UnityContainer();
+            Container.RegisterType<ICache, UnityCache>();
+            Container.RegisterType<ITorrentClientProcessStartInfoProvider, UnityTorrentClientProcessStartInfoProvider>();
+        }
+    }
+}
